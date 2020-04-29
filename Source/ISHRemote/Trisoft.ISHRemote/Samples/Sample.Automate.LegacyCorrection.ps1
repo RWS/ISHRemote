7b911db042dbfbb1dc1f7ed2d9ed3df8b13ca45f
GLOBAL\otokar <otokar@sdl.com>
2020-04-29 11:40:01 +0300

    #97 Write an example Legacy Correction script

--- Source/ISHRemote/Trisoft.ISHRemote/Samples/Sample.Automate.LegacyCorrection.ps1
+++ Source/ISHRemote/Trisoft.ISHRemote/Samples/Sample.Automate.LegacyCorrection.ps1
@@ -1,92 +1,262 @@
-﻿$DebugPreference="SilentlyContinue"
-$VerbosePreference="SilentlyContinue"
+﻿<#
+.SYNOPSIS
+	For all the content objects after filtering re-submit blob.
 
-# Preferably create a user specifically for this conversion operation, this way a restart is possible
-# Provide simple Add-IshUser based on admin user example here
+.DESCRIPTION
+		
+	The powershell script executes the following scenario:
+	1) Get all folders(where type is "ISHModule", "ISHMasterDoc" or "ISHLibrary") recursively under the Data base folder
+    2) Iterate over folders and get objects(where FSTATUS is "To be translated", FISHLASTMODIFIEDBY is not current user and FSOURCELANGUAGE is not empty)
+	3) For every object get blob and submit it again
+	
+	-Log file is provided continuously as the script executes
+	-ISHRemote 0.11.0.1 minimal version is required to run this script.
+	
+	-You can re-run the script and avoid processing of the objects that were already processed. For this task it is important 
+		to fill in values in the "User defined variables" section, depending on Tridion Docs server's authentication type.
+	
+.PARAMETER WsBaseUrl
+	Tridion Docs web services url
 
+.PARAMETER IshUserName
+    Tridion Docs user name
 
+.PARAMETER IshPassword
+     Tridion Docs user password
+#>
+Param(
+	[Parameter(Mandatory=$true)]
+	[string]$WsBaseUrl,
+	[Parameter(Mandatory=$false)]
+	[string]$IshUserName,
+   	[Parameter(Mandatory=$false)]
+	[string]$IshPassword
+)
 
+Function CreateOrUpdateInternalUser{
+<#
+.DESCRIPTION
+	Create or update internal SDL Tridion Docs user.
+	By default all existing user groups are added to the user in "Create" and "Update" operations
+#>
+param(
+	[Parameter(Mandatory=$true)] [Trisoft.ISHRemote.Objects.Public.IshSession]$ishSession,
+	[Parameter(Mandatory=$true)] [string]$userName,
+	[Parameter(Mandatory=$false)] [string]$userPassword,
+	[Parameter(Mandatory=$false)] [string]$userRoles
+)
+	# Get all user groups and add them to the user
+	$ishUserGroups = Find-IshUserGroup -IshSession $ishSession -ActivityFilter "None" -RequestedMetadata (Set-IshRequestedMetadataField -IshSession $ishSession -Name "FISHUSERGROUPNAME" -Level "None")
+	[array]$arrUserGroups = @()
+	$ishUserGroups | % {$arrUserGroups += $_.fishusergroupname}
+	$userGroups = $arrUserGroups -join ", "
 
+	# Find if user already exists
+	$ishUserLovs = Get-IshLovValue -IshSession $ishsession -LovId "USERNAME"
+	$foundUserLov = $ishUserLovs | where {[string]$_.Label -eq $userName}
+	if($foundUserLov -ne $null)
+	{
+		Write-Host "Updating user $userName"
+		$userMetadataUpdate = Set-IshMetadataField -IshSession $ishsession -Name "NAME" -Level None -Value $userName |
+							  Set-IshMetadataField -IshSession $ishSession -Name "FUSERGROUP" -Level "none" -ValueType "Value" -Value $userGroups 
+		if($userPassword -ne "")
+		{
+			$userMetadataUpdate = $userMetadataUpdate | Set-IshMetadataField -IshSession $ishsession -Name "PASSWORD" -Level None -Value $userPassword
+		}
+		if($userRoles -ne "")
+		{
+			$userMetadataUpdate = $userMetadataUpdate | Set-IshMetadataField -IshSession $ishsession -Name "FISHUSERROLES" -Level None -Value $userRoles
+		}
+		$ishUserUpdate = Set-IshUser -IshSession $ishsession -Id $foundUserLov.IshRef -Metadata $userMetadataUpdate
+		return $ishUserUpdate
+	}else{
+		Write-Host "Creating user $userName"
+		
+		$userMetadataCreate = Set-IshMetadataField -IshSession $ishSession -Name "FISHUSERTYPE" -Level "none" -Value "Internal" `
+								| Set-IshMetadataField -IshSession $ishSession -Name "FISHUSERDISPLAYNAME" -Level "none" -Value "$userName" `
+								| Set-IshMetadataField -IshSession $ishSession -Name "FISHUSERLANGUAGE" -Level "none" -Value "en" `
+								| Set-IshMetadataField -IshSession $ishSession -Name "FUSERGROUP" -Level "none" -ValueType "Value" -Value $userGroups `
+								| Set-IshMetadataField -IshSession $ishSession -Name "FISHUSERROLES" -Level "none" -ValueType "Value" -Value $userRoles `
+								| Set-IshMetadataField -IshSession $ishSession -Name "PASSWORD" -Level "none" -Value $userPassword
+		
+		$ishUserCreate = Add-IshUser -IshSession $ishsession `
+									-Name $userName `
+									-Metadata $userMetadataCreate
+		return $ishUserCreate
+	}
+}
 
-#
-# AUXILIARY FUNCTIONS
-#
-Function Convert-ISHCustomDocumentObj {
+Function Log-IshDocumentObj{
+<#
+.DESCRIPTION
+	Log information about the provided ishDocumentObj and action taken on it to the file in the .csv format.
+#>
+param(
+	[Parameter(Mandatory=$true,ValueFromPipeline=$true)] [Trisoft.ISHRemote.Objects.Public.IshDocumentObj]$ishDocumentObj,
+	[Parameter(Mandatory=$true)] [string]$actionName,
+	[Parameter(Mandatory=$true)] [string]$logFileFullPath,
+	[Parameter(Mandatory=$false)] [string]$exceptionMessage
+)
+	$logObject = new-object PSObject
+	$logObject | Add-Member -MemberType NoteProperty -Name "Action" -Value $actionName
+	$logObject | Add-Member -MemberType NoteProperty -Name "LogicalId" -Value $ishDocumentObj.IshRef
+	$logObject | Add-Member -MemberType NoteProperty -Name "Version" -Value $ishDocumentObj.version_version_value
+	$logObject | Add-Member -MemberType NoteProperty -Name "Language" -Value $ishDocumentObj.doclanguage
+	$logObject | Add-Member -MemberType NoteProperty -Name "Status" -Value $ishDocumentObj.fstatus
+	$logObject | Add-Member -MemberType NoteProperty -Name "LastModifiedBy" -Value $ishDocumentObj.fishlastmodifiedby
+	$logObject | Add-Member -MemberType NoteProperty -Name "LastModifiedOn" -Value $ishDocumentObj.fishlastmodifiedon
+	$logObject | Add-Member -MemberType NoteProperty -Name "Exception" -Value $exceptionMessage
+	
+	try{
+		$logObject | Export-Csv -Path $logFileFullPath -NoTypeInformation -Append -Confirm:$false
+	}catch{
+		Write-Host "Exception on appending to the log file $logFileFullPath"
+        Write-Host $_.Exception.Message
+	}
+
+}
+
+Function Convert-ISHCustomDocumentObj{
 <#
 .DESCRIPTION
 	Generic template Convert cmdlet to handle the incoming pipeline objects to your desire. Best practice is to have a WhatIf implementation to see which objects you are handling in what way.
 
 	This could be a custom implementation to get the file and resubmit it you over (new) IWriteMetadata*-plugin or simply purge pretranslation tags. Or to remove certain language cards.
-	
-.PARAMETER TestDescription
-	Mandatory parameter where you should describe what you are trying to get out of the test. Suggestions are client and source descriptions, hardware changes, network changes, etc
-
-.EXAMPLE
-	Provides a valid ISHRemote IshSession. Triggers all available 'MeasureCmdlet' tests available and export them to a CSV file.
-
-	Set-ISHMeasureVariable -Name ([ISHMeasureVariableEnum]::ISHMeasureIshSession) -Value (New-IshSession -WsBaseUrl 'https://example.com/ISHWS/' -IshUserName 'admin' -IshPassword 'admin')
-	Invoke-ISHMeasure -TestDescription "All localhost testing" -MeasureType 'All' -CsvFilePath "C:\temp\ISHMeasure.csv"
-
 #>
-[cmdletbinding(SupportsShouldProcess=$True)]
+[cmdletbinding(SupportsShouldProcess=$true)]
 param(
-	[Parameter(Mandatory=$True,ValueFromPipeline=$True,ParameterSetName='IshObjectsGroup')]
-	$IshObject
+	[Parameter(Mandatory=$true)] [Trisoft.ISHRemote.Objects.Public.IshSession]
+	$ishSession,
+	[Parameter(Mandatory=$true,ValueFromPipeline=$true)] 
+	[Trisoft.IshRemote.Objects.Public.IshDocumentObj] $ishDocumentObj,
+	[Parameter(Mandatory=$true)] 
+	[string] $logFileFullPath
 )
-Process
+	$documentDescription = "$($ishDocumentObj.IshRef)=$($ishDocumentObj.version_version_value)=$($ishDocumentObj.doclanguage)"
+	Write-Host "$documentDescription"
+	
+	# additional requested metadata for the retrieved document objects
+	$requestedMetadata = Set-IshRequestedMetadataField -IshSession $ishSession -Name "FISHLASTMODIFIEDBY" -Level Lng -ValueType Value |
+						 Set-IshRequestedMetadataField -IshSession $ishSession -Name "FISHLASTMODIFIEDON" -Level Lng -ValueType Value |
+						 Set-IshRequestedMetadataField -IshSession $ishSession -Name "FSTATUS" -Level Lng -ValueType Value
+	try{
+		$ishDocumentObj = $ishDocumentObj | Get-IshDocumentObj -IshSession $ishSession -RequestedMetadata $requestedMetadata -IncludeData
+		if ($PSCmdlet.ShouldProcess("$documentDescription", $MyInvocation.MyCommand) -eq $true)
+    	{
+			$ishDocumentObjUpdated = $ishDocumentObj | Set-IshDocumentObj -IshSession $ishSession -Confirm:$false
+		}
+		$ishDocumentObjUpdated | Log-IshDocumentObj -actionName $MyInvocation.MyCommand `
+													-logFileFullPath $logFileFullPath												
+	}
+	catch{
+		Write-Host "Exception"
+	    Write-Host "========="
+	    Write-Host $_.Exception.Message
+	    Write-Host "========="
+		$ishDocumentObj | Log-IshDocumentObj -actionName $MyInvocation.MyCommand `
+											 -logFileFullPath $logFileFullPath `
+											 -exceptionMessage $_.Exception.Message
+	}
+}
+
+CLS
+Import-Module ISHRemote -MinimumVersion 0.11.0.1 -DisableNameChecking
+# Create session
+$ishSession = New-IshSession -wsBaseUrl $WsBaseUrl -IshUserName $IshUserName -IshPassword $IshPassword -IgnoreSslPolicyErrors
+
+#region User defined variables
+$folderTypeFilter = @("ISHModule", "ISHMasterDoc", "ISHLibrary")
+$documentObjectStatusFilter = "To be translated"
+
+#[Re-running script]: If using InfoshareSTS authentication fill in dedicated user details
+$dedicatedIshUserName = "SDLDocsConversionUser"
+$dedicatedIshUserPassword = "SDLDocsConversionUser"
+$dedicatedIshUserRoles = "Reviewer, Author, Translator"
+
+#[Re-running script]: If using ActiveDirectory authentication fill in modified-on filter
+# objects with LastModifiedOn < (Now - $hoursFilter) will be modified
+[int]$hoursFilter = 0
+# define server datetime culture
+$LocaleServer = New-Object System.Globalization.CultureInfo("nl-BE")
+# define datetime
+$dtModifiedOnLessThan = (Get-Date).addHours($hoursFilter).ToString("dd/MM/yyyy HH:mm:ss", $LocaleServer)
+#endregion
+
+#log file location and timestamp
+$logFileLocation = Split-Path -parent $MyInvocation.MyCommand.Definition
+$logTimestamp = get-date -Format "yyyyMMddHHmmss"
+$logFileFullPath = "$logFileLocation\$($logTimestamp)ObjUpdateLog.csv"
+$logFile = New-Item -Path $logFileFullPath -ItemType File -Force
+
+# Get all folders
+$ishFolders = Get-IshFolder -IshSession $ishSession -BaseFolder Data -Recurse -FolderTypeFilter $folderTypeFilter
+if($ishFolders.Count -eq 0)
 {
-    $ishObjectName = ($IshObject.ftitle_logical_value+"="+$IshObject.IshRef+"="+$IshObject.version_version_value+"="+$IshObject.doclanguage)
-    $ishObjectStatus = ($IshObject.fstatus_lng_element)
-    Write-Host ("Handling object["+$ishObjectName+"] of status["+$ishObjectStatus+"]")
-    if (-not $PSCmdlet.ShouldProcess($IshObject))
-    {
-        Write-Host ("What if: Performing the operation on object["+$ishObjectName+"] of status["+$ishObjectStatus+"]")
-        return
-    }
-
-    #try {
-    #} catch 
-    {
-        # Write-Error like transactions or checkedout
-        # write to log file
-    }# at least log with exact error
-
-    #get blob, submit blob #pretranslation or legacy conversion
-    #remove-documentobj In Translation/To be Translated
-
-    #Get-IshDocumentObj -IncludeData | Set-IshDocumentObj
+	Write-Host "No folders matching filter were found."
+	Exit
 }
+else{
+	Write-Host "Found" $ishFolders.count "folders of type: $folderTypeFilter"
 }
 
+# Filtering on content objects (Get-IshFolderContent)
+$metadataFilter = Set-IshMetadataFilterField -IshSession $ishSession -Name "FSTATUS" -Level Lng -Value $documentObjectStatusFilter -ValueType Value |
+				  Set-IshMetadataFilterField -IshSession $ishSession -Name "FSOURCELANGUAGE" -Level Lng -ValueType Value -FilterOperator NotEmpty
 
-#
-# MAIN
-#
+#region Additional filtering to save time when re-running the script
+#Decide on the additional filtering: LastModifiedOn or LastModifiedBy
+if($IshPassword -eq "")
+{ 
+	# typically means Active Directory authentication, ISHRemote can't login as an internal user
+	# additional filtering on LastModifiedOn
+	$metadataFilter = $metadataFilter | Set-IshMetadataFilterField -IshSession $ishSession -Name "FISHLASTMODIFIEDON" -Level Lng -Value $dtModifiedOnLessThan -FilterOperator LessThan -ValueType Value
 
-<# Querying DocumentObjs to find hanging In Translations that were send out for 'Quoting' but never came back
-$metadataFilter = Set-IshMetadataFilterField -Level Lng -Name FSTATUS -ValueType Element -FilterOperator In -Value VSTATUSINTRANSLATION
-Get-IshFolder -FolderPath "\General\Mobile Phones Demo" -Recurse | 
-Get-IshFolderContent -VersionFilter LATEST -LanguagesFilter ('en','de') -MetadataFilter $metadataFilter |
-Convert-ISHCustomDocumentObj -WhatIf
-#>
+}else{
+	# typically means InfoshareSTS authentication, ISHRemote can create user and log in
+	# Create dedicated user
+	$dedicatedIshUser = CreateOrUpdateInternalUser -ishSession $ishSession -userName $dedicatedIshUserName -userPassword $dedicatedIshUserPassword -userRoles $dedicatedIshUserRoles
+	if($dedicatedIshUser -eq $null)
+	{
+		Write-Host "Error on Create/Update of the dedicated SDL Tridion Docs user" 
+		Exit
+	}
 
-<# Querying DocumentObjs to remove target language stubs
-$metadataFilter = Set-IshMetadataFilterField -Level Lng -Name FSTATUS -ValueType Element -FilterOperator In -Value 'VSTATUSTOBETRANSLATED, VSTATUSINTRANSLATION' |
-                  Set-IshMetadataFilterField -Level Lng -Name FSOURCELANGUAGE -FilterOperator NotEmpty
-Get-IshFolder -FolderPath "\General\Mobile Phones Demo" -Recurse | 
-Get-IshFolderContent -MetadataFilter $metadataFilter |
-Convert-ISHCustomDocumentObj -WhatIf
-#>
+	# Create session for the dedicated user
+	$ishSession = New-IshSession -wsBaseUrl $WsBaseUrl -IshUserName $dedicatedIshUserName -IshPassword $dedicatedIshUserPassword -IgnoreSslPolicyErrors
 
-<# Querying DocumentObjs that are currently checked out
-#>
+	# additional filtering on LastModifiedBy
+	$metadataFilter = $metadataFilter | Set-IshMetadataFilterField -IshSession $ishSession -Name "FISHLASTMODIFIEDBY" -Level Lng -Value $dedicatedIshUserName -ValueType Value -FilterOperator NotEqual
+}
+#endregion
 
-<# Querying DocumentObjs that are changed last week
-#>
+# Limit the number of objects retrieved per one web service call 
+$ishSession.MetadataBatchSize = 100
+# Decrease number of default metadata fields requested
+$ishSession.DefaultRequestedMetadata = "Descriptive"
 
-# Querying DocumentObjs for LatestVersion source language if you have a (new) IWriteMetadata*-plugin to update logical metadata
-$metadataFilter = Set-IshMetadataFilterField -Level Lng -Name FSOURCELANGUAGE -FilterOperator Empty |
-                  Set-IshMetadataFilterField -Level Lng -Name FISHLASTMODIFIEDBY -FilterOperator My user
-Get-IshFolder -FolderPath "\General\Mobile Phones Demo" -Recurse | 
-Get-IshFolderContent -VersionFilter LATEST -MetadataFilter $metadataFilter |
-Convert-ISHCustomDocumentObj
+# Loop over folders
+foreach($ishFolder in $ishFolders)
+{
+	$ishObjects = Get-IshFolderContent -IshSession $ishSession `
+									   -IshFolder $ishFolder `
+									   -MetadataFilter $metadataFilter
+	
+	$folderPath = ($ishFolder.fishfolderpath).Replace($ishSession.Separator, $ishSession.FolderPathSeparator) + $ishSession.FolderPathSeparator + $ishFolder.fname
+	if($ishObjects.Count -eq 0)
+	{
+		Write-Host "$folderPath : no objects returned"
+		continue;
+	}
+	
+	Write-Host "$folderPath : $($ishObjects.Count) object(s)"
+	foreach($ishObject in $ishObjects)
+	{
+		$ishObject | Convert-ISHCustomDocumentObj -ishSession $ishSession -logFileFullPath $logFileFullPath
+		#$ishObject | Convert-ISHCustomDocumentObj -ishSession $ishSession -logFileFullPath $logFileFullPath -WhatIf
+		#$ishObject | Convert-ISHCustomDocumentObj -ishSession $ishSession -logFileFullPath $logFileFullPath -Confirm
+
+	}
+}
+
+Remove-Module ISHRemote
+Write-Host "End.`r`nLog file location: $logFileFullPath"
